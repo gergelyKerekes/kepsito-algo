10f7bb258feb768696524680dfd6f596010f7333
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-04-29 10:19:12 +0200

    null check is red

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,20 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits (String input) {
+        int sum = 0;
+
+        if (input.length() == 0 || input == null ) {
+            return -1;
+        } else {
+            for (int i = 0; i < input.length(); i++) {
+                if (Character.isDigit(input.charAt(i))) {
+                    sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+                }
+            }
+        }
+
+        return sum;
+    }
 }
