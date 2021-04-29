3c549f146f4c7909d2311c390851f438c0f768f0
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-04-29 10:26:35 +0200

    project finished

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -5,7 +5,10 @@ public class AddDigits {
     public int addDigits (String input) {
         int sum = 0;
 
-        if (input.length() == 0 || input == null ) {
+        if (input == null) {
+            return -1;
+        }
+        if (input.length() == 0) {
             return -1;
         } else {
             for (int i = 0; i < input.length(); i++) {
